4e5cfce3056141a2af5cd0ce50c5fa0524d962f4
Hamlet Hakobyan <hamlethakobyan@workfront.com>
2015-10-13 22:40:56 +0400

    Added modal dialog and ui.bootstral typeahead controls.

--- AddNoteToWorkfrontWeb/AppRead/Controllers/login-index-controller.js
+++ AddNoteToWorkfrontWeb/AppRead/Controllers/login-index-controller.js
@@ -1,1 +1,8 @@
-﻿
+﻿angular.module('workfront-addin')
+.controller('LoginController', ['$scope', function($scope) {
+    $scope.username = '';
+    $scope.password = '';
+    $scope.url = '';
+
+    $('#myModal').modal({ backdrop: 'static' });
+}])
--- AddNoteToWorkfrontWeb/AppRead/Controllers/update-index-controller.js
+++ AddNoteToWorkfrontWeb/AppRead/Controllers/update-index-controller.js
@@ -1,66 +1,67 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
     var app = angular.module('workfront-addin');
-    app.requires.push('ngSanitize', 'ui.select');
+    app.requires.push('ngAnimate', 'ui.bootstrap');
 
-    app.controller('UpdateController', [
-        '$scope', '$http', '$location', '$timeout', function ($scope, $http, $location, $timeout) {
+    app.controller('UpdateController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
             $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-            $scope.disabled = undefined;
-            $scope.searchEnabled = undefined;
+            $scope.groupFunc = function (item) {
+                var objName;
+                if (item.ObjCode === 'PROJ') {
+                    objName = 'Projects';
+                } else if (item.ObjCode === 'TASK') {
+                    objName = 'Tasks';
+                } else if (item.ObjCode === 'OPTASK') {
+                    objName = 'Issues';
+                } else {
+                    objName = 'Unknowon Object';
+                }
 
-            $scope.enable = function() {
-                $scope.disabled = false;
+                item.obj_name = objName;
+                return objName;
             };
 
-            $scope.disable = function() {
-                $scope.disabled = true;
-            };
 
-            $scope.enableSearch = function() {
-                $scope.searchEnabled = true;
-            }
-
-            $scope.disableSearch = function() {
-                $scope.searchEnabled = false;
-            }
-
-            $scope.clear = function() {
-                $scope.person.selected = undefined;
-                $scope.address.selected = undefined;
-                $scope.country.selected = undefined;
-            };
-
-            $scope.groupFunc = function(item) {
-                if (item.ObjCode === 'PROJ')
-                    return 'Projects';
-                if (item.ObjCode === 'TASK')
-                    return 'Tasks';
-                if (item.ObjCode === 'OPTASK')
-                    return 'Issues';
-                return 'Unknowon Object';
-            };
-
-            $scope.namedEntities = [{ Name: 'No match found' }];
-            $scope.refreshNamedEntities = function (term) {
+            $scope.selected = undefined;
+            // Any function returning a promise object can be used to load values asynchronously
+            $scope.getEntities = function (term) {
                 if ($.cookie('workfront-session')) {
                     return $http.post('../../api/get/containssearch', '=' + term)
-                        .then(
-                            function (response) {
-                                if (response.data.length === 0) {
-                                    $scope.namedEntities = [{ Name: 'No match found' }];
-                                } else {
-                                    $scope.namedEntities = response.data;
-                                }
+                        .then(function(response) {
+                                var data = response.data;
+                                _(data).groupBy(groupFunc)
+                                    .map(function(g) {
+                                        _.each(g, function(e, i) {
+                                            //          console.log(e);
+                                            var isFirst = false;
+                                            if (i === 0) {
+                                                isFirst = true;
+                                            }
+
+                                            e.is_first = isFirst;
+                                        });
+
+                                    })
+                                    .flatten()
+                                    .map(function(e) {
+                                        return {
+                                            id: e.ID,
+                                            name: e.Name,
+                                            group_name: e.Obj_name,
+                                            is_first: e.is_first
+                                        };
+                                    })
+                                    .value();
                             },
-                            function (error) {
+                            function(error) {
                                 var e = error;
                             });
 
                 } else {
                     $location.path('/login');
+                    return {};
                 }
             };
         }
